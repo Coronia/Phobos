23714b0256bfa243dc9f239e691a876909625b77
Meta (X230T) <crabiter@vivaldi.net>
2021-03-09 00:27:51 +0200

    First hooks for TurretOffset

--- src/Ext/TechnoType/Hook.cpp
+++ src/Ext/TechnoType/Hook.cpp
@@ -6,6 +6,7 @@
 #include "Body.h"
 #include "../BulletType/Body.h"
 #include "../Techno/Body.h"
+#include "../../ExtraHeaders/Matrix3D.h"
 
 DEFINE_HOOK(6F64A9, HealthBar_Hide, 5)
 {
@@ -106,3 +107,50 @@ DEFINE_HOOK(6F9E50, TechnoClass_Update, 5)
 
 	return 0;
 }
+
+DEFINE_HOOK(6F3C56, TechnoClass_Transform_6F3AD0_TurretMultiOffset, 0)
+{
+	LEA_STACK(Matrix3D*, mtx, 0xD8 - 0x90);
+	GET(TechnoTypeClass*, technoType, EDX);
+
+	mtx->Translate(technoType->TurretOffset, 0.0, 0.0);
+
+	return 0x6F3C6D;
+}
+
+DEFINE_HOOK(6F3E6E, FootClass_firecoord_6F3D60_TurretMultiOffset, 0)
+{
+	LEA_STACK(Matrix3D*, mtx, 0xCC - 0x90);
+	GET(TechnoTypeClass*, technoType, EBP);
+
+	mtx->Translate(technoType->TurretOffset, 0.0, 0.0);
+
+	return 0x6F3E85;
+}
+
+DEFINE_HOOK(73B780, UnitClass_DrawVXL_MultiTurretOffset0, 0)
+{
+	GET(TechnoTypeClass*, technoType, EAX);
+
+	if (!technoType->TurretOffset) {
+		return 0x73B78A;
+	}
+
+	return 0x73B790;
+}
+
+DEFINE_HOOK(73BA4C, UnitClass_DrawVXL_MultiTurretOffset1, 0)
+{
+	LEA_STACK(Matrix3D*, mtx, 0x1D0 - 0x13C);
+	GET(TechnoTypeClass*, technoType, EBX);
+
+	double& factor = *reinterpret_cast<double*>(0xB1D008);
+
+	mtx->Translate(technoType->TurretOffset * factor, 0.0, 0.0);
+
+	return 0x73BA68;
+}
+//73C88A
+
+
+
